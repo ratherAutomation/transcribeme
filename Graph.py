e9d56d8821e9a85dff013c5b71ffd8767083b399
ratherAutomation <145386758+ratherAutomation@users.noreply.github.com>
2023-09-19 21:00:24 -0300

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -7,7 +7,7 @@ import requests
 from io import StringIO
 
 # URL cruda del archivo CSV en GitHub (reemplaza con la URL de tu archivo)
-url_csv_raw = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/mi_archivo.csv'
+url_csv_raw = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/active_users_by_day_top_10.csv'
 
 # Hacer una solicitud HTTP para obtener el contenido del archivo CSV
 response = requests.get(url_csv_raw)
@@ -26,10 +26,10 @@ app = dash.Dash(__name__)
 server = app.server
 # Definir el diseño de la aplicación
 app.layout = html.Div([
-    html.H1("Gráfico de Prueba"),
+    html.H1("TranscribeMe Dashboard"),
     dcc.Graph(
         id='grafico-prueba',
-        figure=px.scatter(df, x='x', y='y', title='Gráfico de dispersión de prueba')
+        figure=px.scatter(df, x='Date', y='ActiveUsers', title='Active Users by Date')
     )
 ])
 
