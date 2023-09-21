5ae1f7567008f6776e048474b350867e6b724e59
ratherAutomation <145386758+ratherAutomation@users.noreply.github.com>
2023-09-21 10:31:47 -0300

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -9,7 +9,7 @@ import requests
 from io import StringIO
 
 # URL cruda del archivo CSV en GitHub (reemplaza con la URL de tu archivo)
-url_csv_raw = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/active_users_by_day_top_10.csv'
+url_csv_raw = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/subsixty.csv'
 
 # Hacer una solicitud HTTP para obtener el contenido del archivo CSV
 response = requests.get(url_csv_raw)
@@ -26,30 +26,66 @@ else:
 # Crear una aplicación Dash
 app = dash.Dash(__name__)
 server = app.server
-# Definir el diseño de la aplicación
+
+# Definir las opciones para el filtro desplegable de países
+opciones_paises = [{'label': pais, 'value': pais} for pais in recent_subs['Country'].unique()]
+
+# Diseñar la interfaz de usuario de la aplicación
 app.layout = html.Div([
-    html.H1("TranscribeMe Dashboard"),
-    
-    # Dropdown para seleccionar país
     dcc.Dropdown(
-        id='country-dropdown',
-        options=[{'label': country, 'value': country} for country in df['Country'].unique()],
-        value=df['Country'].unique()[0]  # Valor predeterminado
+        id='filtro-pais',
+        options=opciones_paises,
+        value='Argentina',  # País seleccionado por defecto
+        multi=False
     ),
-    
-    # Gráfico de barras
-    dcc.Graph(id='bar-chart')
+    dcc.Graph(id='grafico-nuevos-subscriptores')
 ])
 
-# Callback para actualizar el gráfico de barras según el país seleccionado
+# Definir la función de actualización del gráfico
 @app.callback(
-    Output('bar-chart', 'figure'),
-    Input('country-dropdown', 'value')
+    dash.dependencies.Output('grafico-nuevos-subscriptores', 'figure'),
+    [dash.dependencies.Input('filtro-pais', 'value')]
 )
-def update_bar_chart(selected_country):
-    filtered_df = df[df['Country'] == selected_country]
-    fig = px.bar(filtered_df, x='Date', y='ActiveUsers', title=f'Usuarios Activos por Fecha en {selected_country}')
+def actualizar_grafico(pais_seleccionado):
+    # Filtrar los datos por el país seleccionado
+    df_filtrado = recent_subs[recent_subs['Country'] == pais_seleccionado]
+
+    # Crear el gráfico de barras
+    fig = px.bar(df_filtrado, x='date', y='new_subscribers', title=f'Nuevos Subscriptores por Fecha en {pais_seleccionado}')
+
+    # Agregar una línea vertical en la fecha 2023-09-13
+    fecha_cambio = '2023-09-13'
+    fig.add_shape(
+        go.layout.Shape(
+            type="line",
+            x0=fecha_cambio,
+            x1=fecha_cambio,
+            y0=0,
+            y1=max(df_filtrado['new_subscribers']),
+            line=dict(color="black", width=4,dash='dash'),
+        )
+    )
+    fig.add_annotation(
+        go.layout.Annotation(
+            text="3 days free trial",
+            x=fecha_cambio,
+            y=max(df_filtrado['new_subscribers']),
+            showarrow=True,
+            arrowhead=1,
+            ax=-50,
+            ay=-30
+        )
+    )
+    # Personalizar el diseño del gráfico
+    fig.update_layout(
+        xaxis_title='Fecha',
+        yaxis_title='Nuevos Subscriptores',
+        showlegend=False  # Para ocultar la leyenda si no se necesita
+    )
+
     return fig
+
+# Ejecutar la aplicación
 
 if __name__ == '__main__':
     app.run_server(debug=True)
