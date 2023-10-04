ef5cf570e08ce55508152777481e32907ebef57a
ratherAutomation <145386758+ratherAutomation@users.noreply.github.com>
2023-10-04 16:15:25 -0300

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -13,6 +13,7 @@ import plotly.graph_objects as go
 # URL cruda del archivo CSV en GitHub (reemplaza con la URL de tu archivo)
 url_csv_raw = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/subs_sixty.csv'
 url_csv_dau_sub = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/ratio_df.csv'
+url_csv_balance = 'https://raw.githubusercontent.com/ratherAutomation/transcribeme/main/income_expense_balance.csv'
 
 # Hacer una solicitud HTTP para obtener el contenido del archivo CSV
 response = requests.get(url_csv_raw)
@@ -22,7 +23,7 @@ if response.status_code == 200:
     recent_subs = pd.read_csv(StringIO(response.text))
 else:
     print("No se pudo obtener el archivo CSV")
-# Hacer una solicitud HTTP para obtener el contenido del archivo CSV
+
 response_two = requests.get(url_csv_dau_sub)
 # Verificar si la solicitud fue exitosa
 if response_two.status_code == 200:
@@ -30,7 +31,13 @@ if response_two.status_code == 200:
     ratio_df = pd.read_csv(StringIO(response_two.text))
 else:
     print("No se pudo obtener el archivo CSV")
-
+    
+response_three = requests.get(url_csv_balance)
+if response_three.status_code == 200:
+    # Leer el contenido del archivo CSV en un DataFrame
+    income_expenses_balance = pd.read_csv(StringIO(response_three.text))
+else:
+    print("No se pudo obtener el archivo CSV")
 
 # Crear una aplicación Dash
 app = dash.Dash(__name__)
@@ -100,9 +107,53 @@ app.layout = html.Div([
         ], style={'width': '40%', 'display': 'inline-block'}),  # Ajusta el ancho según tus necesidades
     ], style={'width': '100%', 'display': 'flex'}),  # Ajusta el ancho según tus necesidades
 ])
-
-
-# Definir la función de actualización del gráfico
+#0000000
+    html.Div([
+        html.H1("balance"),
+        dcc.Dropdown(
+            id='country-filter',
+            options=[{'label': country, 'value': country} for country in income_expenses_balance['country'].unique()],
+            value=income_expenses_balance['country'].unique()[0]  # Valor predeterminado
+        ),
+        dcc.Graph(id='graph')
+    ])
+    
+    @app.callback(
+        Output('graph', 'figure'),
+        Input('country-filter', 'value')
+    )
+def update_graph(selected_country):
+    filtered_df = income_expenses_balance[income_expenses_balance['country'] == selected_country]
+    fig = px.scatter(
+        filtered_df,
+        x='cost',
+        y='expected_average_income',
+        title=f'associeted costs and expected revenue for: {selected_country}'
+    ) 
+    
+    fig.update_traces(
+        text=filtered_df['labels'],
+        hovertemplate='<b>Date</b>: %{text}<br><b>Cost</b>: %{x}<br><b>Expected Avg Income</b>: %{y}',  # Posición y tamaño de las etiquetas
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=income_expenses_balance['cost'],  # Puedes ajustar estos valores según tu necesidad
+            y=1 * income_expenses_balance['cost'],  # Pendiente de 0.05
+            mode='lines',
+            name='y = x',  # Nombre de la recta en la leyenda
+            line=dict(color='black', dash='dash')  # Personalizar el estilo de la línea
+        )
+    )
+            
+    max_axis_val = max([filtered_df['cost'].max(),filtered_df['expected_average_income'].max()])*1.1
+        
+    fig.update_xaxes(range=[0, max_axis_val]) #filtered_df['cost'].min()
+    fig.update_yaxes(range=[0, max_axis_val]) #filtered_df['expected_average_income'].min()
+    
+    return fig
+        
+    
+    # Definir la función de actualización del gráfico
 @app.callback(
     dash.dependencies.Output('grafico-nuevos-subscriptores', 'figure'),
     [dash.dependencies.Input('filtro-pais', 'value')]
@@ -110,43 +161,43 @@ app.layout = html.Div([
 def actualizar_grafico(pais_seleccionado):
     # Filtrar los datos por el país seleccionado
     df_filtrado = recent_subs[recent_subs['country'] == pais_seleccionado]
-
-    # Crear el gráfico de barras
+    
+        # Crear el gráfico de barras
     fig = px.bar(df_filtrado, x='date', y='new_subs', title=f'Nuevos Subscriptores por Fecha en {pais_seleccionado}')
-
-    # Agregar una línea vertical discontinua en la fecha 2023-09-13 con un título
+    
+        # Agregar una línea vertical discontinua en la fecha 2023-09-13 con un título
     fecha_cambio = '2023-09-13'
     fig.add_shape(
         go.layout.Shape(
-            type="line",
-            x0=fecha_cambio,
-            x1=fecha_cambio,
-            y0=0,
-            y1=max(df_filtrado['new_subs']),
-            line=dict(color="grey", width=2, dash='dash'),
+        type="line",
+        x0=fecha_cambio,
+        x1=fecha_cambio,  
+        y0=0,
+        y1=max(df_filtrado['new_subs']),
+        line=dict(color="grey", width=2, dash='dash'),
         )
     )
-
-    # Agregar un título a la línea vertical
+    
+        # Agregar un título a la línea vertical
     fig.add_annotation(
         go.layout.Annotation(
-            text="3 days free trial",
-            x=fecha_cambio,
-            y=max(df_filtrado['new_subs']),
-            showarrow=True,
-            arrowhead=1,
-            ax=-50,
-            ay=-30
+        text="3 days free trial",
+        x=fecha_cambio,
+        y=max(df_filtrado['new_subs']),
+        showarrow=True,
+        arrowhead=1,
+        ax=-50,
+        ay=-30
         )
     )
-
-    # Personalizar el diseño del gráfico
+    
+        # Personalizar el diseño del gráfico
     fig.update_layout(
         xaxis_title='Fecha',
         yaxis_title='Nuevos Subscriptores',
         showlegend=False  # Para ocultar la leyenda si no se necesita
     )
-
+    
     return fig
 
 
